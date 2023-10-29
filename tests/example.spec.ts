63ba44009fce2a79208230a7661ac8bec863addb
Paweł Gawlik <pgawlik215@gmail.com>
2023-10-29 13:21:58 +0100

    fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,9 @@
 import { test, _electron as electron } from '@playwright/test'
-
 test('has title', async () => {
-  const electronApp = await electron.launch({ args: ['out/main/index.js'] })
+  const electronApp = await electron.launch({
+    args: ['out/main/index.js'],
+    env: { ...process.env, NODE_ENV: 'development' }
+  })
   // Evaluation expression in the Electron context.
   const appPath = await electronApp.evaluate(async ({ app }) => {
     // This runs in the main Electron process, parameter here is always
@@ -12,7 +14,7 @@ test('has title', async () => {
   const window = await electronApp.firstWindow()
   // Print the title.
   console.log(await window.title())
-  await window.waitForTimeout(20000)
   // Exit app.
   await electronApp.close()
+  await window.close()
 })
