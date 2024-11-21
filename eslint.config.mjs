7346306bef65629fe80ac0191c92a783a634d144
Pawel <pgawlik@opera.com>
2024-11-21 16:01:29 +0100

    eslint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,34 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import electronEslint from '@electron-toolkit/eslint-config-ts'
+import electronPrettier from '@electron-toolkit/eslint-config-prettier'
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ['.js', '.mjs', '.cjs', '.mjs', '.ts', '.cts', '.mts'],
-    ignores: ['node_modules', 'dist', 'out', '.gitignore'],
-    parserOptions: {
-      extraFileExtensions: ['.svelte']
+    files: ['**/*.{js,mjs,cjs,ts}']
+  },
+  {
+    ignores: ['node_modules', 'dist', 'out', '.gitignore']
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { extraFileExtensions: ['.svelte'] }
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:svelte/recommended',
-      '@electron-toolkit/eslint-config-ts/recommended',
-      '@electron-toolkit/eslint-config-prettier'
-    ],
-    overrides: [
-      {
-        files: ['*.svelte'],
-        parser: 'svelte-eslint-parser',
-        parserOptions: {
-          parser: '@typescript-eslint/parser'
-        }
-      }
-    ],
-    rules: {
-      'svelte/no-unused-svelte-ignore': 'off'
+    files: ['*.svelte'],
+    parser: 'svelte-eslint-parser',
+    parserOptions: {
+      parser: '@typescript-eslint/parser'
     }
-  }
+  },
+  {
+    plugins: {
+      '@electron-toolkit/eslint-config-ts': electronEslint,
+      '@electron-toolkit/eslint-config-prettier': electronPrettier
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
 ]
